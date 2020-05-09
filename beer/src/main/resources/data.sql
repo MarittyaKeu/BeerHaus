2b59595838d9a24bca6b6740517f4dd1bf8cf6b2
Sokthai <sokthaitang@gmail.com>
2020-05-08 22:19:32 -0400

    create insert statement for wine table

--- beer/src/main/resources/data.sql
+++ beer/src/main/resources/data.sql
@@ -6,3 +6,15 @@ values (1, 'Corona', 4.6, 'Pale Ale');
 insert into beer
 (id, name, alcohol_by_volume, beer_type)
 values (2, 'Tree House - Haze', 8.2, 'Double IPA');
+
+
+
+
+
+insert into wine
+(id, name, alcohol_by_volume, wine_type)
+values (1, 'Tiger', 12.3, 'red wine');
+
+insert into wine
+(id, name, alcohol_by_volume, wine_type)
+values (2, 'ABC', 1.3, 'white water');
