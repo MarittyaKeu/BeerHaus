9f7b4e7a00aa3b32082f0746d81cdff88c100d1f
Sokthai <sokthaitang@gmail.com>
2020-05-28 20:43:42 -0400

    create styling for product page

---
+++ beer/client/src/components/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+function main(){
+    return(
+        <div className="main">
+            <a href="/beers/getAllBeers" className="alcohol">Beers</a>
+            <a href="/wines/getAllwines" className="alcohol">Wines</a>
+
+        </div>
+    )
+}
