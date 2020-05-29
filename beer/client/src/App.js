1229dc17e7ac4fdd896dc56a21723fb18dd51c80
Sokthai <sokthaitang@gmail.com>
2020-05-28 20:42:42 -0400

    add main and product page

--- beer/client/src/App.js
+++ beer/client/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import axios from 'axios';
 import Product from './components/product';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import './style/style.css';
+import Beer from '../src/components/product';
+
+
+
+
 
 function App() {
 
@@ -21,15 +25,12 @@ function App() {
 
 
   return (
-    <div className="App">
-      <Product type="wine"/>
-      <Route exact path='/' component={Product}/>
-{/* 
-      <form onSubmit={e=> onSubmit(e)}>
-        <input type="submit" value="start"/>
-       </form>
-       */}
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/beer/:id' component={Product}/>
+        <Route exact path='/beer/:id' component={Product}/>
+      </Switch>
+    </Router>
   );
 }
 
--- beer/client/src/components/product.jsx
+++ beer/client/src/components/product.jsx
@@ -3,43 +3,78 @@ import axios from 'axios';
 
 
 
-function product(){
-    // const [prod, setProd] = useState(0)
+const Product = (props) => {
+    const [prod, setProd] = useState({
+        picture: 'corona.jpeg',
+        name: 'corona lime',
+        type: 'alcho',
+        volume: '900',
+        description: 'Corona Extra wery Cervecería Modelo Extra wery Cervecería Modelo Extra wery Cervecería Modelo Extra wery Cervecería Modelo and owned by Belgian company AB InBev. It is commonly served with a wedge of lime or lemon in the neck of the bottle to add tartness and flavour',
+        date: new Date().getDate(),
+        length: 1
+    });
 
-    // const getProd = () => {
-    //     axios.get("/wines/getAllWines")
-    //     .then( res => {
-    //         console.log(res);
-    //     })
-    //     .catch( err =>{
-    //         console.error(err)
-    //     }
-    //     )    
-    // }
+    const getProd = () => {
+        setProd({
+            picture: propps[1].picture,
+            name: propps[1].name,
+            type: propps[1].type,
+            volume: propps[1].volume,
+            description: propps[1].description,
+            date: propps[1].date,
+            length: 1
+        })
+    }
 
-
+ 
 
 
     return(
         <Fragment>
+            <div className="container">
+                <div className="picture">
+                    <img src={require(`../../public/media/${prod.picture}`)} alt="" srcSet=""/>
+                </div>
 
-        </Fragment>
-        // <div className="container">
-        //     <div className="picture">
-        //         <img src="" alt="" srcset=""/>
-        //     </div>
+                <div className="desc">
+                    <div className="item">
+                        <div className="left">Name: </div>
+                        <div className="right">{prod.name}</div>
+                    </div>
 
-        //     <div className="desc">
-        //         <h2 className="name"></h2>
-        //         <h4 className="type"></h4>
-        //         <h4 className="volume"></h4>
+                    <div className="item">
+                        <div className="left">type: </div>
+                        <div className="right">{prod.type}</div>
+                    </div>
 
+                    <div className="item">
+                        <div className="left">volume: </div>
+                        <div className="right">{prod.volume}</div>
+                    </div>
+
+                    <div className="item">
+                        <div className="left">Founded date: </div>
+                        <div className="right">{prod.date}</div>
+                    </div>
+
+                    <div className="item">
+                        <div className="left ">description: </div>
+                        <div className="right description">{prod.description}</div>
+                    </div>
+
+                    <div className="btn">
+                        <button className="next" onClick={getProd()} >Next</button>
+
+                    </div>
+
+                      
+
+                </div>
                 
-        //         <h4 className="description"></h4>
-        //         <h6 className="date"></h6>
-        //     </div>
-        // </div>
+            </div>
+        </Fragment>
+        
     )
 }
 
-export default product;
+export default Product;
